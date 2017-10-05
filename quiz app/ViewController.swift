8a0c553aa72d50b5f1910197c01c744a79dfe96b
kevin578 <kbriggs@aapl000894.local>
2017-10-05 08:22:25 -0400

    added buttons

--- quiz app/ViewController.swift
+++ quiz app/ViewController.swift
@@ -9,16 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//variables for every piece of text that is going to change - labels and buttons
+    
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+   
+//all my functions that are triggered when the button is pressed
+    
+    @IBAction func button1(_ sender: Any) {
+        
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+        
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
