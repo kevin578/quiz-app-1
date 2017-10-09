09eecea3e3b36fe99f0fade704700ce54fdab032
kevin578 <kbriggs@2600-6c64-607f-f8a6-0c42-a10c-4f35-1bab.dhcp6.chtrptr.net>
2017-10-09 17:00:10 -0400

    everything is functional

--- quiz app/ViewController.swift
+++ quiz app/ViewController.swift
@@ -22,56 +22,82 @@ class ViewController: UIViewController {
     
     var correctAnswer = [1, 0, 2]
     var questionNumber = 0
+
 //variables for every piece of text that is going to change - labels and buttons
     
     @IBOutlet weak var question: UILabel!
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
-   
+    @IBOutlet weak var nextQuestion: UIButton!
+    
 //all my functions that are triggered when the button is pressed
+    
+    @IBAction func nextQuestion(_ sender: Any) {
+        if (questionNumber < questions.count - 1){
+            questionNumber += 1
+        }
+        else {
+            questionNumber = 0
+        }
+
+        displayQuestion(questionNumber: questionNumber)
+        button1.isHidden = false
+        button2.isHidden = false
+        button3.isHidden = false
+        nextQuestion.isHidden = true
+        displayQuestion(questionNumber: questionNumber)
+    }
+    
     
     @IBAction func button1(_ sender: Any) {
         checkAnswer(button: 0)
-        questionNumber += 1
-        displayQuestion(questionNumber: questionNumber)
+
         
     }
     
+
     @IBAction func button2(_ sender: Any) {
         checkAnswer(button: 1)
-        questionNumber += 1
-        displayQuestion(questionNumber: questionNumber)
+
     }
     
     @IBAction func button3(_ sender: Any) {
         checkAnswer(button: 2)
-        questionNumber += 1
-        displayQuestion(questionNumber: questionNumber)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        nextQuestion.isHidden = true
         displayQuestion(questionNumber: 0)
 
     }
     
     func checkAnswer(button: Int) {
+        button1.isHidden = true
+        button2.isHidden = true
+        button3.isHidden = true
+        nextQuestion.isHidden = false
     if (button == correctAnswer[questionNumber]) {
-        print("Correct")
+        question.text = "Correct"
     }
     else {
-        print("Wrong")
+        question.text = "Wrong"
     }
+        if (questionNumber < questions.count - 1){
+            nextQuestion.setTitle("Next Question", for: .normal)
+        }
+        else {
+            nextQuestion.setTitle("Restart", for: .normal)
+        }
+        
     }
     
     func displayQuestion(questionNumber: Int) {
-        if (questionNumber < questions.count) {
         question.text = questions[questionNumber]
         button1.setTitle(answers[questionNumber][0], for: .normal)
         button2.setTitle(answers[questionNumber][1], for: .normal)
         button3.setTitle(answers[questionNumber][2], for: .normal)
-        }
     }
 
 
